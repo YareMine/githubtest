75406bb15e8ae950b494cc53221f27b0d7ecf3ee
YareMine <15712317531@163.com>
2020-09-27 09:05:14 +0800

    第一次提交到本地库

---
+++ src/main/java/com/atguigu/github/TestGitHub.java
@@ -0,0 +1,14 @@
+package com.atguigu.github;
+
+/**
+ * @author Chong Wang
+ * @create 2020-09-27 8:51
+ */
+public class TestGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("hello, GitHub!");
+        System.out.println("修改后未add");
+    }
+
+}
